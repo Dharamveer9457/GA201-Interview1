54c556f27aed677385966cf2a4ab67f4370f90aa
Dharamveer <dharammveer945734@outlook.com>
2023-09-25 18:03:30 +0530

    Interview Done

---
+++ reverseString.py
@@ -0,0 +1,22 @@
+# Function to return reversed string
+def reverseString(str):
+    reverse = ""
+    for el in str:
+        reverse = el + reverse
+    return reverse
+    
+res = reverseString('Python is fun')
+print(res)
+
+# Function to find the max salary in the list of employees.
+def max_salary_employee(employees):
+    max_salary_employee = employees[0]
+    max_salary = max_salary_employee.get("salary",0)
+
+    for el in employees:
+        salary = el.get("salary",0)
+        if salary>max_salary:
+            max_salary = salary
+            max_salary_employee = el
+        
+    return max_salary_employee
